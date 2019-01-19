84a99837d50c36137295b366022339adda3669e6
shuwen123 <shuwenroy@yahoo.com>
2019-01-19 14:57:24 -0800

    version 1.0
    finished hello world sample

--- fass_inspector_python/Hello.py
+++ fass_inspector_python/Hello.py
@@ -6,11 +6,8 @@
 # AWS Lambda function with JSON as input and 
 # output
 
-import json
 import logging
-import os
-import subprocess
-import re
+import Hello_Register
 
 # my_handler is the lambda function on aws that will be called
 # event: is the JSON object or message that passed in
@@ -18,45 +15,8 @@ import re
 # return JSON with customized fields.
 
 def my_handler(event, context):
-#     # using if else
-#     inputKeyset = list(event.keys())
-#     if 'name' in inputKeyset:
-        # name = event['name']
-        # message = 'Hello {} from lambda!'.format(name)  
-        # return {
-        #         'message' : message,
-        #         'name' : name
-        #         }
-#     else:
-        # logger = logging.getLogger()
-        # logger.setLevel(logging.ERROR)
-        # logger.error('THE INPUT JSON DOESN\'T HAVE SPECIFIED FIELD.')
-        # return {
-        #         'error': 'please check log'
-        #         }
-    # using os.popen() for linux command
-    f = os.popen('grep \'btime\' /proc/stat')
-    temp = f.read().strip()
-    vmbt = temp.replace('btime ', '')
-    p = os.popen('grep \'model name\' /proc/cpuinfo | head -1')
-    result = p.read()
-    temp = re.sub('[\n\t]','', result)
-    cpuType = temp.replace('model name: ', '')
-
-    # using try catch
-    try:
-        name = event['name']
-        message = 'Hello {} from lambda!'.format(name)  
-        return {
-                'message' : message,
-                'name' : name,
-                'CPU' : cpuType,
-                'vmUpTime' : vmbt 
-                }
-    except:
-        logger = logging.getLogger()
-        logger.setLevel(logging.ERROR)
-        logger.error('THE INPUT JSON DOESN\'T HAVE SPECIFIED FIELD.')
-        return {
-                'error': 'please check log and input JSON'
-                }
+    vmbt = Hello_Register.getVmuptime()
+    cpuType = Hello_Register.getCpuType()
+    myUuid, newContainer = Hello_Register.stampVM()
+    vmSpec = Hello_Register.profileVM(event, vmbt, cpuType, myUuid, newContainer)
+    return vmSpec
