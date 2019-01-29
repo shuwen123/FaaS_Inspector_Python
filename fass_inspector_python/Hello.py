e633091d7c2c804c4b50bc9c3d6501309d97106d
shuwen123 <shuwenroy@yahoo.com>
2019-01-28 19:45:28 -0800

    with local test bash
    invoke via cmdline bash tests added

---
+++ fass_inspector_python/Hello.py
@@ -0,0 +1,31 @@
+# TCSS 499
+# Wen Shu
+# Winter 2019
+
+# This is just a Hello world python script for
+# AWS Lambda function with JSON as input and 
+# output
+
+import logging
+import Register
+
+# my_handler is the lambda function on aws that will be called
+# event: is the JSON object or message that passed in
+# context: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+# return JSON with customized fields.
+
+def my_handler(event, context):
+    vmSpec = Register.profileVM()
+    try:
+        name = event['name']
+        message = 'Hello {} from lambda!'.format(name)  
+        vmSpec['message'] = message
+        vmSpec['name'] = name
+        return vmSpec
+    except:
+        logger = logging.getLogger()
+        logger.setLevel(logging.ERROR)
+        logger.error('THE INPUT JSON DOESN\'T HAVE SPECIFIED FIELD.')
+        return {
+                'error': 'please check log and input JSON'
+                }
---
+++ fass_inspector_python/Register.py
@@ -0,0 +1,63 @@
+# TCSS 499
+# Wen Shu
+# Winter 2019
+
+# This is just library for profiling the VM's
+# spec which your program is running on. It
+# targets AWS Lambda for now.
+
+import json
+import logging
+import os
+import subprocess
+import re
+import uuid
+import shlex
+
+def __getUpTime():
+    # https://www.quora.com/Whats-the-difference-between-os-system-and-subprocess-call-in-Python
+    # using subprocess.check_output() for linux command
+    cliInput = 'grep btime /proc/stat'
+    args = shlex.split(cliInput)
+    # this returns a bytes object, so I need to decode it into a string.
+    f = subprocess.check_output(args).decode('utf-8')
+    # temp = f.communicate()
+    return int(f.strip().replace('btime ',''))
+
+def __getVmCpuStat():
+    # using os.popen() for linux command
+    p = os.popen('grep \'model name\' /proc/cpuinfo | head -1')
+    result = p.read()
+    temp = re.sub('[\n\t]','', result)
+    return temp.replace('model name: ', '')
+
+def __stampContainer():
+    myUuid = ''
+    newContainer = 1
+    if os.path.isfile('/tmp/container-id'):
+        stampFile = open('/tmp/container-id', 'r')
+        stampID = stampFile.readline()
+        myUuid = stampID
+        stampFile.close()
+        # print('im here! read uuid from file!')
+        newContainer = 0
+    else:
+        stampFile = open('/tmp/container-id', 'w')
+        myUuid = str(uuid.uuid4()) # uuid4() generates a random uuid, uuid is not str
+        stampFile.write(myUuid)
+        stampFile.close()
+        # print('im here! write uuid to the file!')
+    return myUuid, newContainer
+
+# event: is the JSON object or message that passed in.
+# return JSON/Dictionary with customized fields.
+def profileVM():
+    vmbt = __getUpTime()
+    cpuType = __getVmCpuStat()
+    myUuid, newContainer = __stampContainer()
+    return {
+                'CPU' : cpuType,
+                'vmuptime' : vmbt,
+                'uuid' : myUuid,
+                'newcontainer' : newContainer
+                }
